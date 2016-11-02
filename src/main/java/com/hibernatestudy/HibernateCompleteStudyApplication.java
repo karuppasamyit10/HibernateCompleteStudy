91a3e019765a50badad5e7732ad2256d715a9283
KARUPPASAMY MARIAPPAN <karuppasamyit10@gmail.com>
2016-11-02 14:46:43 +0530

    Initial Project setup

---
+++ src/main/java/com/hibernatestudy/HibernateCompleteStudyApplication.java
@@ -0,0 +1,12 @@
+package com.hibernatestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateCompleteStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateCompleteStudyApplication.class, args);
+	}
+}
