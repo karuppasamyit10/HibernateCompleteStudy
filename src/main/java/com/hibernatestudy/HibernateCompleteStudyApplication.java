d3929fa9f8ced67ffe3d313f05c87eea613dc59f
KARUPPASAMY MARIAPPAN <karuppasamyit10@gmail.com>
2016-11-16 21:25:59 +0530

    Author name created in main class

--- src/main/java/com/hibernatestudy/HibernateCompleteStudyApplication.java
+++ src/main/java/com/hibernatestudy/HibernateCompleteStudyApplication.java
@@ -3,6 +3,12 @@ package com.hibernatestudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author Karuppu
+ * @created on Nov 16, 2016
+ * HibernateCompleteStudy
+ */
 @SpringBootApplication
 public class HibernateCompleteStudyApplication {
 
